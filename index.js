d2b0731c482025cba05116226e5c23e13d0258a7
Alizada_Rahim <abdul_rahim.alizada@novartis.com>
2020-04-13 18:27:30 +0200

    ___

--- index.js
+++ index.js
@@ -20,34 +20,18 @@ app.get("/api/users", (req, res) => res.status(200).send(users));
 /**get a specific user by id */
 app.get("/api/users/:id", (req, res) => {
     const user = users.find(user => user.id === parseInt(req.params.id))
-    if(!user) res.status(404).send(`
-    <div style="text-align: center; border: 0.5px solid lightGray; border-radius: 6px;">
-        <h1 style="font-size: 2rem">404</h1>
-        <h1 style="color: red;">Not Found!</h1>
-    </div>
-    `);
+    if(!user) return res.status(404).send(`Bad Request`);
     res.status(200).send(user);
 });
 
 
 /**add a new user */
 app.post('/api/users', (req, res) => {
+    //Calling the Validator function and getting the error using object destructuring
+    const { error } = validateUser(req.body); 
+    if (error) return res.status(400).send(result.error.details[0].message);
 
-    //validating the request body 
-    const { error } = validateUser(req.body); //Object destructuring
-
-    if (error) {
-      res.status(400).send(result.error.details[0].message);
-      return;
-    }
-
-    const newUser = {
-        id: users.length + 1,
-        name: req.body.name,
-        job: req.body.job
-    }
-
-    //adding the new user
+    const newUser = { id: users.length + 1,name: req.body.name,job: req.body.job}
     users.push(newUser);
     res.status(200).send(newUser);
 });
@@ -56,39 +40,36 @@ app.post('/api/users', (req, res) => {
 
 /**Updating a User info */
 app.put('/api/users/:id', (req, res) => {
-    //checking if the user exists
     const user = users.find(user => user.id === parseInt(req.params.id));
-    if (!user) res.status(404).send('Not Found!');
-    //validating the request body 
+    if (!user) return res.status(404).send('Not Found!');
+
     const { error } = validateUser(req.body);
-    if (error) {
-      res.status(400).send(result.error.details[0].message);
-      return;
-    }
-    //Updating the user 
+    if (error) return res.status(400).send(result.error.details[0].message);
+  
     user.name = req.body.name;
     user.job = req.body.job;
     res.status(200).send(user);
 });
 
-function validateUser(user) {
-    const schema = Joi.object({
-      name: Joi.string().min(3).max(30).required(),
-      job: Joi.string().min(3).max(30).required(),
-    });
-
-    return schema.validate(user);
-}
 
 /**Removing a user by id*/
 app.delete('/api/users/:id', (req, res) => {
-  //checking if the user exists
   const user = users.find(user => user.id === parseInt(req.params.id));
-  if (!user) res.status(404).send('Not Found!');
-  //removing user
+  if (!user) return res.status(404).send('Not Found!');
+
   const index = users.indexOf(user);
   const result = users.splice(index, 1);
   res.status(200).send(result);
 });
+
+
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    job: Joi.string().min(3).max(30).required(),
+  });
+
+  return schema.validate(user);
+}
 
 app.listen(port, () => console.log(`listening on port ${port}...`))
