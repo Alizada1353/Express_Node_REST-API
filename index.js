2e9c712516da525b3f7759ba5cc6288e6e17b638
Alizada_Rahim <abdul_rahim.alizada@novartis.com>
2020-04-13 14:23:28 +0200

    getting all users with a single query string

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000;
+
+const users = [
+  { id: 1, name: "John Doe", job: "Manager" },
+  { id: 2, name: "Alex Brand", job: "Business analyst" },
+  { id: 3, name: "Mary Luther", job: "Security advisor" },
+];
+
+app.get('/', (req, res) => res.send('connection established!'))
 
 
-app.get('/', (req, res) => res.send('Wellcome!'))
+app.get('/api/users', (req, res) => res.send(users))
 
-const port = process.env.PORT || 3000
+
+
+
+
 app.listen(port, () => console.log(`listening on port ${port}...`))
