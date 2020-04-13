9020c5411b06020a24127f1e8a22c48b794ace17
Alizada_Rahim <abdul_rahim.alizada@novartis.com>
2020-04-13 17:51:03 +0200

    refactoring validation using a fuctions

--- index.js
+++ index.js
@@ -16,30 +16,6 @@ app.get("/", (req, res) => res.status(200).send("connection established!"));
 /**get all users */
 app.get("/api/users", (req, res) => res.status(200).send(users));
 
-/**add a new user */
-app.post('/api/users', (req, res) => {
-
-    const schema = Joi.object({
-      name: Joi.string().min(3).max(30).required(),
-      job: Joi.string().min(3).max(30).required(),
-    });
-
-    const result = schema.validate(req.body);
-
-    if(result.error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-
-    const newUser = {
-        id: users.length + 1,
-        name: req.body.name,
-        job: req.body.job
-    }
-
-    users.push(newUser);
-    res.status(200).send(newUser);
-});
 
 /**get a specific user by id */
 app.get("/api/users/:id", (req, res) => {
@@ -54,18 +30,38 @@ app.get("/api/users/:id", (req, res) => {
 });
 
 
+/**add a new user */
+app.post('/api/users', (req, res) => {
+
+    //validating the request body 
+    const { error } = validateUser(req.body); //Object destructuring
+
+    if (error) {
+      res.status(400).send(result.error.details[0].message);
+      return;
+    }
+
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        job: req.body.job
+    }
+
+    //adding the new user
+    users.push(newUser);
+    res.status(200).send(newUser);
+});
+
+
+
 /**Updating a User info */
 app.put('/api/users/:id', (req, res) => {
     //checking if the user exists
     const user = users.find(user => user.id === parseInt(req.params.id));
     if (!user) res.status(404).send('Bad Request');
     //validating the request body 
-    const schema = Joi.object({
-      name: Joi.string().min(3).max(30).required(),
-      job: Joi.string().min(3).max(30).required(),
-    });
-    const result = schema.validate(req.body);
-    if (result.error) {
+    const { error } = validateUser(req.body);
+    if (error) {
       res.status(400).send(result.error.details[0].message);
       return;
     }
@@ -74,5 +70,14 @@ app.put('/api/users/:id', (req, res) => {
     user.job = req.body.job;
     res.status(200).send(user);
 });
+
+function validateUser(user) {
+    const schema = Joi.object({
+      name: Joi.string().min(3).max(30).required(),
+      job: Joi.string().min(3).max(30).required(),
+    });
+
+    return schema.validate(user);
+}
 
 app.listen(port, () => console.log(`listening on port ${port}...`))
